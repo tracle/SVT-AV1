26a4e1bbdfe63fbbb41fcaa69bf1544bd1c9c17c
hguermaz <hassen.guermazi@intel.com>
2020-04-06 14:08:03 -0700

    updated

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -40,7 +40,7 @@ extern "C" {
 #define UNIFIED_PART 1
 #define UNIFIED_FILTERING          1
 #define UNIFIED_TF                 1
-#define UNIFIED_NICS               1
+#define UNIFIED_NICS               0
 
 #define COMMON_16BIT 1 // 16Bit pipeline support for common
 #if UNIFIED_FILTERING
